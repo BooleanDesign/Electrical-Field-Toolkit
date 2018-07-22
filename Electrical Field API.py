aeba720d9335172b5d81fa486c80e80efea0fa0a
BooleanDesign <NathanDigginsbms@gmail.com>
2018-07-22 00:23:15 -0600

    Update Electrical Field API.py to 1.2 Alpha
    Update 1.2 Alpha features:
         - New and Improved UI
         - Improved Program Flow (see bug fixes)
         - Title and Icon on Matplotlib
    Major Bug Fixes
         - Resolved input issues in Config_path, x_pad, y_pad, Image_path, saving sub_plots
         - Resolved glitch which caused Charges list to change after passing Continue
         - Resolved glitch requiring the User to input 2x Continue at the beginning of the program.
         - Build in redundancies for various data type errors and input errors etc.
         - Many other significant bug and programming fixes

--- Electrical Field API.py
+++ Electrical Field API.py
@@ -17,13 +17,17 @@ import tkinter as tk
 import ttk as ttkstyle
 from matplotlib import style
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+"""
+Program setup
+"""
+version = '1.1 Alpha'
+count = 0
+os.system('Title Electric Field API %s' % (version))
 image_directory = os.getcwd() + '\\Images\\'
 Configurations_path = os.getcwd() + '\\Configurations\\'
-x_pad = 0.0 #System settings for saving individual subplots
+x_pad = 0.0  # System settings for saving individual subplots
 y_pad = 0.0
-print Configurations_path
 plt.switch_backend('TkAgg')
-version = '1.0.2 Alpha'
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(121)
 div = make_axes_locatable(ax1)
@@ -31,6 +35,8 @@ cax = div.append_axes('right', '5%', '5%')
 ax2 = fig1.add_subplot(122, projection='3d')
 mng = plt.get_current_fig_manager()
 mng.window.state('zoomed')
+mng.window.wm_iconbitmap("bin\\EFAPIicon.ico")
+fig1.canvas.set_window_title('Electric Field API %s' %(version))
 q = 2
 
 
@@ -192,20 +198,41 @@ def get_inputs(base_charges, base_resolution):
     input_finished = False
     charges = base_charges
     resolution = base_resolution
+    if count <= 1:
+        # Fixes Continue glitch
+        return charges, resolution
+    else:
+        pass
     while input_finished is False:
         os.system('cls')
-        print_charges(charges)
         correct_input = False
         while correct_input is False:
+            os.system('cls')
+            print_charges(charges)
             correct_input = True
             selection = raw_input("Add, Advanced, Export, Import, Exit, Delete, Save, Continue: ")
             if selection == 'add' or selection == 'Add' or selection == 'new':
                 try:
-                    charges.append(Charge(float(raw_input('What is the x position of the charge? ')),
-                                          float(raw_input("What is the y position of the charge? ")),
-                                          float(raw_input('What is the particle\'s Charge? '))))
+                    new_charge = Charge(float(raw_input('What is the x position of the charge? ')),
+                                        float(raw_input("What is the y position of the charge? ")),
+                                        float(raw_input('What is the particle\'s Charge? ')))
+                    if new_charge.charge == 0.0:
+                        raise FloatingPointError()
+                    elif ([new_charge.x,new_charge.y] in [[i.x,i.y] for i in charges]) == True:
+                        raise LookupError()
+                    else:
+                        charges.append(new_charge)
+                except LookupError:
+                    print "Point Charge was in same location as another charge."
+                    waiter = raw_input('Press any key to continue: ')
+                    correct_input = False
+                except FloatingPointError:
+                    print "Point Charge cannot have a charge of 0 coulomb."
+                    waiter = raw_input('Press any key to continue: ')
+                    correct_input = False
                 except ValueError:
                     print "Error 0001: Input must be a floating point number."
+                    waiter = raw_input('Press any key to continue: ')
                     correct_input = False
             elif selection == "Advanced" or selection == 'advanced' or selection == 'options':
                 advanced_loop = False
@@ -215,20 +242,21 @@ def get_inputs(base_charges, base_resolution):
                           '[4]Administrator Settings\n[Enter] Back...'
                     adv_selection = raw_input('Which option to you wish to configure? ')
                     if adv_selection == '0' or adv_selection == 'Change Configuration Path':
-                        os.system('cls')
                         config_path_loop = False
                         while config_path_loop == False:
+                            os.system('cls')
                             print "Current Configuration Path: (%s) Current Working Directory: (%s) " % (
-                            Configurations_path, os.getcwd())
+                                Configurations_path, os.getcwd())
                             new_path = raw_input("New Configuration Path (Must be from CWD): ")
-                            if os.path.isdir(os.getcwd() + new_path) == True:
+                            if os.path.isdir(new_path) == True:
                                 Configurations_path = new_path
                                 config_path_loop = True
                             elif new_path == 'back':
                                 config_path_loop = True
                             else:
-                                print "Input failed, the path was not existant."
+                                print "Input failed, the path was not existent."
                                 config_path_loop = False
+                                waiter = raw_input('Press any key to continue: ')
                     elif adv_selection == '1' or adv_selection == "System Info":
                         os.system('cls')
                         print "Publisher: Boolean Designs"
@@ -249,21 +277,28 @@ def get_inputs(base_charges, base_resolution):
                         os.system('cls')
                         plot_choice_loop = False
                         while plot_choice_loop == False:
+                            os.system('cls')
                             try:
                                 plot_choice = int(raw_input('Which subplot do you want to save? '))
                                 if plot_choice == 1:
                                     extent = ax1.get_window_extent().transformed(fig1.dpi_scale_trans.inverted())
-                                    fig1.savefig(image_directory+raw_input('Image file name (.png): '),bbox_inches=extent.expanded(1+x_pad,1+y_pad))
+                                    fig1.savefig(image_directory + raw_input('Image file name (.png): '),
+                                                 bbox_inches=extent.expanded(1 + x_pad, 1 + y_pad))
                                     plot_choice_loop = True
                                 elif plot_choice == 2:
                                     extent = ax2.get_window_extent().transformed(fig1.dpi_scale_trans.inverted())
-                                    fig1.savefig(image_directory+ raw_input('Image file name (.png): '),
+                                    fig1.savefig(image_directory + raw_input('Image file name (.png): '),
                                                  bbox_inches=extent.expanded(1 + x_pad, 1 + y_pad))
                                     plot_choice_loop = True
                                 else:
                                     print "That is not an eligible subplot."
+                                    waiter = raw_input('Press any key to continue: ')
+                            except ValueError:
+                                print "That is not an eligible subplot."
+                                waiter = raw_input('Press any key to continue: ')
                             except IOError:
                                 print "Illegal filename, or directory doesn't exist."
+                                waiter = raw_input('Press any key to continue: ')
                                 plot_choice_loop = False
                     elif adv_selection == '4' or adv_selection == 'Advanced Settings':
                         advanced_settings_loop = False
@@ -274,15 +309,24 @@ def get_inputs(base_charges, base_resolution):
                             if advanced_setting_input == '0' or advanced_setting_input == 'X Padding':
                                 os.system('cls')
                                 print "Current configured X Padding variable: %s." % (str(x_pad))
-                                x_pad = float(raw_input('New X_Pad value: '))
+                                try:
+                                    x_pad = float(raw_input('New X_Pad value: '))
+                                except ValueError:
+                                    print "Input must be a number."
+                                    waiter = raw_input('Press any key to continue: ')
                             elif advanced_setting_input == '1' or advanced_setting_input == 'Y Padding':
                                 os.system('cls')
                                 print "Current configured X Padding variable: %s." % (str(y_pad))
-                                y_pad = float(raw_input('New Y_Pad value: '))
+                                try:
+                                    y_pad = float(raw_input('New Y_Pad value: '))
+                                except ValueError:
+                                    print "Input must be a number."
+                                    waiter = raw_input('Press any key to continue: ')
                             elif advanced_setting_input == '2' or advanced_setting_input == 'Image Directory':
                                 os.system('cls')
                                 config_impath_loop = False
                                 while config_impath_loop == False:
+                                    os.system('cls')
                                     print "Current Image Path: (%s) Current Working Directory: (%s) " % (
                                         image_directory, os.getcwd())
                                     new_path = raw_input("New Configuration Path (Must be from CWD): ")
@@ -294,6 +338,7 @@ def get_inputs(base_charges, base_resolution):
                                     else:
                                         print "Input failed, the path was not existant."
                                         config_impath_loop = False
+                                        waiter = raw_input('Press any key to continue: ')
                             elif advanced_setting_input == '' or advanced_setting_input == 'Back':
                                 advanced_settings_loop = True
                             else:
@@ -312,11 +357,12 @@ def get_inputs(base_charges, base_resolution):
                     print "Exporting current build to the Configurations folder."
                     file_name_loop = False
                     while file_name_loop == False:
+                        os.system('cls')
                         file_name = raw_input('Save the configuration to what file name (.txt)? ')
                         try:
                             if file_name[-4:] != '.txt':
                                 raise SyntaxError()
-                            elif os.path.isfile(Configurations_path+file_name) == True:
+                            elif os.path.isfile(Configurations_path + file_name) == True:
                                 check = raw_input('This file already exists in the directory, overwrite <y/n>? ')
                                 if check == 'y':
                                     file_name_loop = True
@@ -329,11 +375,12 @@ def get_inputs(base_charges, base_resolution):
                                 file_name_loop = True
                         except SyntaxError:
                             print "Filename must have .txt extension to save."
+                            waiter = raw_input('Press any key to continue: ')
                     print "Saving file to %s" % (Configurations_path + file_name)
-                    write_file = open(Configurations_path+file_name,'w').close() #Clears the file
-                    write_file = open(Configurations_path+file_name,'w')
+                    write_file = open(Configurations_path + file_name, 'w').close()  # Clears the file
+                    write_file = open(Configurations_path + file_name, 'w')
                     for i in charges:
-                        write_file.write('%s,%s,%s\n' % (str(i.x),str(i.y),str(i.charge)))
+                        write_file.write('%s,%s,%s\n' % (str(i.x), str(i.y), str(i.charge)))
                     write_file.close()
                     print "File has been written to and saved."
                     waiter = raw_input('Press any key to continue: ')
@@ -348,9 +395,12 @@ def get_inputs(base_charges, base_resolution):
                 print "Found %s data sets in current configurations folder (%s). Configuration file can be changed in " \
                       "advanced settings. " % (str(len(data_sets)), Configurations_path)
                 for file in data_sets:
-                    print '[%s] Name: %s | Ext: %s' % (str(data_sets.index(file)),
-                                                       file.split('.')[0],
-                                                       file.split('.')[1])
+                    if file.endswith('.txt') == True:
+                        print '[%s] Name: %s | Ext: %s' % (str(data_sets.index(file)),
+                                                           file.split('.')[0],
+                                                           file.split('.')[1])
+                    else:
+                        pass
                 """
 
                 Starts the file selection while loop
@@ -382,7 +432,8 @@ def get_inputs(base_charges, base_resolution):
                                     raise SyntaxError("Dataset improperly formatted")
                                 else:
                                     pass
-                                charges.append(Charge(float(temp_charge[0]), float(temp_charge[1]), float(temp_charge[2])))
+                                charges.append(
+                                    Charge(float(temp_charge[0]), float(temp_charge[1]), float(temp_charge[2])))
                                 data_set.close()
                             print "File successfully imported"
                             file_selection_loop = True
@@ -405,7 +456,7 @@ def get_inputs(base_charges, base_resolution):
                 save_file_name_loop = False
                 while save_file_name_loop == False:
                     try:
-                        fig1.savefig(image_directory+raw_input('Image name (.png): '))
+                        fig1.savefig(image_directory + raw_input('Image name (.png): '))
                         save_file_name_loop = True
                     except IOError:
                         print "Directory either doesn't exist, or filename is illegal."
@@ -417,9 +468,14 @@ def get_inputs(base_charges, base_resolution):
                         charges = []
                     else:
                         del (charges[int(number) - 1])
+                except IndexError:
+                    print "That charge doesn't exist."
+                    correct_input = False
+                    waiter = raw_input('Press any key to continue: ')
                 except ValueError:
                     print 'Error 0002: Input must be an integer.'
                     correct_input = False
+                    waiter = raw_input('Press any key to continue: ')
             elif selection == 'Exit' or selection == 'Done' or selection == 'finish' or selection == 'exit':
                 print "Exiting the Electrical Field API"
                 exit()
@@ -433,6 +489,7 @@ def get_inputs(base_charges, base_resolution):
                     correct_input = False
             else:
                 print 'Error 0004: Input was improper. '
+                waiter = raw_input('Press any key to continue: ')
                 correct_input = False
 
 
@@ -503,11 +560,12 @@ def animate(i):
     :param i:
     :return:
     """
+    global count
     inputs = get_inputs(charges, resolution)
+    count += 1
     ax1.cla()
     ax2.cla()
     create_graph(inputs[0], inputs[1])
-
 
 animations = []
 charges = [Charge(0.0, 0.0, 1.0)]
