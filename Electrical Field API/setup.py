359bf5938712eff12242bb70ea61ec266320f9bd
BooleanDesign <nathandigginsbms@gmail.com>
2018-07-23 13:34:08 -0600

    Uploading 1.0 Beta with new .exe extension

--- Electrical Field API/setup.py
+++ Electrical Field API/setup.py
@@ -1,1 +1,18 @@
+import cx_Freeze
+import sys
+import os
+import importlib
+importlib.import_module('mpl_toolkits.mplot3d').Axes3D
 
+base = None
+
+
+executables = [cx_Freeze.Executable("Electric Field API.py", base=base, icon=os.getcwd()+"\\bin\\EFAPIicon.ico")]
+
+cx_Freeze.setup(
+    name = "Electric Field API",
+    options = {"build_exe": {'includes': ['numpy.core._methods','numpy.lib.format','tkFileDialog','FileDialog'], 'packages': ['matplotlib.pyplot',"mpl_toolkits.mplot3d","matplotlib",'Tkinter','FileDialog','tkFileDialog'], "include_files":[os.getcwd()+"\\bin\\EFAPIicon.ico"]}},
+    version = "1.3",
+    description = "Electric Field Visualization",
+    executables = executables
+    )
